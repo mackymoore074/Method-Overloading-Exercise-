61524a87445f4c5da23c06925e01681ae39cd6a8
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-08-27 12:11:26 -0700

    Done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace MethodOverloading
+﻿using System.Net.Http.Headers;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var result = a + b;
+            if (isTrue)
+            {
+                return $"{result} dollars";
+            }
+            else
+            {
+                return $"{result}";
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(5, 1));                  
+            Console.WriteLine(Add(5, 1, true));            
+            Console.WriteLine(Add(1, 16, true));            
+            Console.WriteLine(Add(1, 16, false));
+
         }
     }
 }
